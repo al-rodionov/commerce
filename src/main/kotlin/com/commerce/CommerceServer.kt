09a28984f0d22d36114e9f59475b2a8b9d498991
Aleksandr <ra88011712@gmail.com>
2023-02-13 20:21:58 +0700

    Spring profiles - server/client

--- src/main/kotlin/com/commerce/CommerceServer.kt
+++ src/main/kotlin/com/commerce/CommerceServer.kt
@@ -2,24 +2,49 @@ package com.commerce
 
 import io.grpc.Server
 import io.grpc.ServerBuilder
-import org.springframework.stereotype.Service
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import javax.annotation.PostConstruct
 
-@Service
+@Component
+@Profile("server")
 class CommerceServer {
-        val server: Server = ServerBuilder
-            .forPort(15002)
-            .addService(HelloWorldService())
-            .build()
 
-        @PostConstruct
-        fun start() {
-            server.start()
-        }
+    @Value("\${commerce.server.available.time}")
+    lateinit var serverAvailableTime: String
 
-        private class HelloWorldService : HelloGrpcKt.HelloCoroutineImplBase() {
-            override suspend fun hello(request: HelloRequest) = helloResponse {
-                message = "Hello ${request.message}"
-            }
+    val server: Server = ServerBuilder
+        .forPort(15002)
+        .addService(HelloWorldService())
+        .build()
+
+    @PostConstruct
+    fun start() {
+        planningShutdown()
+
+        server.start()
+    }
+
+    fun planningShutdown() {
+        Runtime.getRuntime().addShutdownHook(Thread {
+            server.shutdown()
+            server.awaitTermination()
+        })
+
+        val executor = Executors.newSingleThreadScheduledExecutor()
+        executor.schedule({
+            server.shutdown()
+            println("Turn down the server")
+            executor.shutdown()
+        }, serverAvailableTime.toLong(), TimeUnit.SECONDS)
+    }
+
+    private class HelloWorldService : HelloGrpcKt.HelloCoroutineImplBase() {
+        override suspend fun hello(request: HelloRequest) = helloResponse {
+            message = "Hello ${request.message}"
         }
+    }
 }
